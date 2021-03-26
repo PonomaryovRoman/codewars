d4eefa82a60ba30affce6f32cea00aeba3f64362
AsahiOcean <asahiocean@icloud.com>
2021-03-26 07:12:46 +0500

    Multiply

---
+++ 8 kyu/Multiply/Multiply.playground/Contents.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+func multiply(_ a: Double, _ b: Double) -> Double {
+    return a * b
+}
+
+import XCTest
+
+class SolutionTest: XCTestCase {
+    static var allTests = [
+        ("testMultiply", testMultiply),
+    ]
+    
+    func testMultiply() {
+        XCTAssertEqual(multiply(1, 3), 3, accuracy:0.0000001)
+        XCTAssertEqual(multiply(0, 3), 0, accuracy:0.0000001)
+        XCTAssertEqual(multiply(-1, 3), -3, accuracy:0.0000001)
+    }
+}
+
+SolutionTest.defaultTestSuite.run()
