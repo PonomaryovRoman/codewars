d7bea92846d17bc72415c44a1342dc377dd46048
AsahiOcean <asahiocean@icloud.com>
2021-03-27 00:44:02 +0500

    Add URL

--- 8 kyu/Grasshopper - Personalized Message/great.playground/Contents.swift
+++ 8 kyu/Grasshopper - Personalized Message/great.playground/Contents.swift
@@ -1,5 +1,7 @@
 import UIKit
 
+// https://www.codewars.com/kata/5772da22b89313a4d50012f7/train/swift
+
 func great(_ name: String, _ owner: String) -> String {
     name == owner ? "Hello boss" : "Hello guest"
 }
