b7df2be16d72e76040c7e9922f72329028b06bee
AsahiOcean <asahiocean@icloud.com>
2021-03-26 07:07:13 +0500

    Reversed Strings

---
+++ 8 kyu/Reversed Strings/README.md
@@ -0,0 +1,6 @@
+# [Reversed Strings](https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/swift)
+
+Complete the solution so that it reverses the string passed into it.
+```swift
+'world'  =>  'dlrow'
+```
---
+++ 8 kyu/Reversed Strings/reverse.playground/Contents.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+func reverse(_ str: String) -> String {
+    return String(str.reversed())
+}
+
+import XCTest
+
+class SolutionTest: XCTestCase {
+    static var allTests = [
+        ("Short words", testShortWords),
+        ("Longer words", testLongerWords)
+    ]
+
+    func testShortWords() {
+        XCTAssertEqual(reverse("hello"), "olleh")
+        XCTAssertEqual(reverse("rat"), "tar")
+        XCTAssertEqual(reverse("alpha"), "ahpla")
+    }
+    
+    func testLongerWords() {
+        XCTAssertEqual(reverse("codewars"), "srawedoc")
+        XCTAssertEqual(reverse("football"), "llabtoof")
+        XCTAssertEqual(reverse("translation"), "noitalsnart")
+    }
+}
+
+SolutionTest.defaultTestSuite.run()
