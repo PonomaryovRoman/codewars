e9e7c3325fccdb36ba5283ef2f1773539b746b0d
AsahiOcean <asahiocean@icloud.com>
2021-03-26 07:02:42 +0500

    Summation

---
+++ 8 kyu/Summation/summation.playground/Contents.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+func summation(_ n: Int) -> Int {
+    // return n * (1 + n) / 2
+    return (1...n).reduce(0){ $0 + $1 }
+}
+
+import XCTest
+
+class SolutionTest: XCTestCase {
+    static var allTests = [
+        ("Basic tests", basicTests),
+    ]
+
+    func basicTests() {
+        XCTAssertEqual(summation(1), 1)
+        XCTAssertEqual(summation(8), 36)
+        XCTAssertEqual(summation(22), 253)
+        XCTAssertEqual(summation(100), 5050)
+        XCTAssertEqual(summation(213), 22791)
+    }
+}
+
+SolutionTest.allTests
