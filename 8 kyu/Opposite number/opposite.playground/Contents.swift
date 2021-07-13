8ddf4716ca3b6eec3a7f1ce7933bb4f5dbcdb35b
ASAHI OCEAN <asahiocean@icloud.com>
2021-07-14 04:13:22 +0500

    Update Contents.swift

--- 8 kyu/Opposite number/opposite.playground/Contents.swift
+++ 8 kyu/Opposite number/opposite.playground/Contents.swift
@@ -1,19 +1,17 @@
-import UIKit
+import Foundation
 
+// Opposite number
 // https://www.codewars.com/kata/56dec885c54a926dcd001095/train/swift
 
 func opposite(number: Double) -> Double {
-    return -(number)
+    return -number
 }
+
 import XCTest
 
 class SolutionTest: XCTestCase {
-    static var allTests = [
-        ("Basic test", testExample),
-    ]
-
-    func testExample() {
-      XCTAssertEqual(opposite(number: 1), -1)
+    func test() {
+        XCTAssertEqual(opposite(number: 1), -1)
     }
 }
 
