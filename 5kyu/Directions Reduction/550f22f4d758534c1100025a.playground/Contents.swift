a77fea3b82378b6153bb276926dbc4c3647d26fe
asahiocean <asahiocean@icloud.com>
2022-01-24 04:59:47 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- 5kyu/Directions Reduction/550f22f4d758534c1100025a.playground/Contents.swift
+++ 5kyu/Directions Reduction/550f22f4d758534c1100025a.playground/Contents.swift
@@ -4,19 +4,23 @@ import Foundation
 // https://www.codewars.com/kata/550f22f4d758534c1100025a/train/swift
 
 func dirReduc(_ arr: [String]) -> [String] {
-    var arr = arr
-    func helper(_ arr: [String]) -> Int? {
-        for value in 0..<arr.count - 1 {
-            let val = (arr[value],arr[value+1])
-            if val.0 == "NORTH" && val.1 == "SOUTH" { return value }
-            if val.0 == "SOUTH" && val.1 == "NORTH" { return value }
-            if val.0 == "EAST" && val.1 == "WEST" { return value }
-            if val.0 == "WEST" && val.1 == "EAST" { return value }
+    var array = arr
+    let helper: ([String]) -> Int? = {
+        for val in 0..<($0.count - 1) {
+            switch ($0[val], $0[val+1]) {
+            case ("NORTH","SOUTH"): return val
+            case ("SOUTH", "NORTH"): return val
+            case ("EAST", "WEST"): return val
+            case ("WEST", "EAST"): return val
+            default: break
+            }
         }
         return nil
     }
-    while let val = helper(arr) { arr.removeSubrange(val...val+1)}
-    return arr
+    while let value = helper(array) {
+        array.removeSubrange(value...value+1)
+    }
+    return array
 }
 
 // MARK: - Solution 2 -
