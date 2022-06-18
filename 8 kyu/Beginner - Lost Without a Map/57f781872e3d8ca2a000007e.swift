5fd51a0d05a3bf508b46eb02e62da4860a2f8102
asahiocean <asahiocean@icloud.com>
2022-06-19 01:05:11 +0500

    57f781872e3d8ca2a000007e.swift
    Beginner - Lost Without a Map
    https://www.codewars.com/kata/57f781872e3d8ca2a000007e/train/swift
    
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

---
+++ 8 kyu/Beginner - Lost Without a Map/57f781872e3d8ca2a000007e.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+// Beginner - Lost Without a Map
+// https://www.codewars.com/kata/57f781872e3d8ca2a000007e/train/swift
+
+func maps(a: Array<Int>) -> Array<Int> {
+    return a.map({ $0 * 2 })
+}
+
+// MARK: - Test Cases -
+
+// Result: Executed 2 tests, with 0 failures (0 unexpected) in 0.007 (0.009) seconds
+
+import XCTest
+
+class Tests: XCTestCase {
+    
+    func test0() {
+        XCTAssertEqual(maps(a: [1,2,3,4,5]), [2,4,6,8,10])
+        XCTAssertEqual(maps(a: [1,-2,3,4,5]), [2,-4,6,8,10])
+        XCTAssertEqual(maps(a: [-1,2,3,4,-5]), [-2,4,6,8,-10])
+    }
+    
+    func test1() {
+        XCTAssertEqual(maps(a: []), [])
+    }
+}
+
+Tests.defaultTestSuite.run()
