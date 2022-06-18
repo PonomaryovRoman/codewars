6b99dc8ad7a6173a4c84fa563cf8bf0c572cfce5
asahiocean <asahiocean@icloud.com>
2022-06-19 01:09:36 +0500

    57f780909f7e8e3183000078.swift
    Beginner - Reduce but Grow
    https://www.codewars.com/kata/57f780909f7e8e3183000078/train/swift
    
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

---
+++ 8 kyu/Beginner - Reduce but Grow/57f780909f7e8e3183000078.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+// Beginner - Reduce but Grow
+// https://www.codewars.com/kata/57f780909f7e8e3183000078/train/swift
+
+func grow(_ arr: [Int]) -> Int {
+    return arr.reduce(1, *)
+}
